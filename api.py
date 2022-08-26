278ba70fabf6079d3808fdb9bdb8b52b9766ba6b
ezequielwish <ezequielalmeida.wish@gmail.com>
2022-08-25 22:35:47 -0300

    first commit

---
+++ api.py
@@ -0,0 +1,27 @@
+from flask import Flask, send_from_directory
+from wish_YT_downloader import Video
+
+path = './download/'
+api = Flask(__name__)  
+
+@api.route('/<video_id>/LQ')
+def low_quality(video_id):
+    video = Video(f'https://www.youtube.com/watch?v={video_id}')
+    filename = video.download('-LQ-')
+    return send_from_directory(path, filename, as_attachment=True)
+
+@api.route('/<video_id>/HQ')
+def high_quality(video_id):
+    video = Video(f'https://www.youtube.com/watch?v={video_id}')
+    filename = video.download('-HQ-')
+    return send_from_directory(path, filename, as_attachment=True)
+
+@api.route('/<video_id>/MP3')
+def in_mp3(video_id):
+    video = Video(f'https://www.youtube.com/watch?v={video_id}')
+    filename = video.download('-MP3-')
+    return send_from_directory(path, filename, as_attachment=True)
+
+
+if __name__ == "__main__":
+    api.run(debug=True)
---
+++ wish_YT_downloader/__init__.py
@@ -0,0 +1,58 @@
+import moviepy.editor as mp
+from pytube import YouTube
+import os
+
+
+class Video:
+    def __init__(self, url):
+        self.url = url
+        self.yt = YouTube(url)
+
+    @staticmethod
+    def format_title(title, no_spaces=False):
+        '''Turns the video title valid to Windows OS filenames'''
+        new = []
+        for char in title: # Separate only valid characters and put on the list
+            if char == ' ':
+                if no_spaces:
+                    new.append('_')
+                else:
+                    new.append(char)
+            elif char == '|':
+                new.append('-')
+            elif char.lower() in (r'aáàãâbcdeéèêfghiíìjklmnopqrstuvwxyz0987654321([{}])/$&~#@!-".,?'):
+                new.append(char)
+        new = ''.join(new).strip() # Convert list into a string
+        if len(new) <= 50: # 50 charactere is a good lenght to filenames
+            return new
+        else:
+            return new[:51]+'...' # Return only the first 50 characteres to bypass Filename Lenght eror
+
+    @staticmethod
+    def convert_mp4_to_mp3(path, filename):
+        with mp.AudioFileClip(f'{path}\{filename}.mp4') as audioclip:
+            audioclip.write_audiofile(f'{path}\{filename}.mp3')
+        os.remove(f'{path}/{filename}.mp4') # Delete the .mp4
+
+    def download(self, quality):
+        '''Download the video in High Quality, Low Quality or MP3'''
+        filename = self.format_title(self.yt.title[:])
+        path = "./download"
+        if quality == '-MP3-':
+            '''The button pressed is Audio MP3, so download as audio .mp4 and convert to .mp3'''
+            video = self.yt.streams.get_audio_only()
+            video.download(path, filename=f'{filename}.mp4') # Download in the specified folder (path)
+            self.convert_mp4_to_mp3(path, filename)
+            filename += '.mp3'
+        elif quality == '-HQ-':
+            '''Select the highest resolution and download them'''
+            filename += ' [HQ].mp4'
+            video = self.yt.streams.get_highest_resolution()
+            video.download(path, filename=f'{filename}')
+        elif quality == '-LQ-':
+            '''Select the lowest resolution and download them'''
+            filename += ' [LQ].mp4'
+            video = self.yt.streams.get_lowest_resolution()
+            video.download(path, filename=f'{filename}')
+        return filename
+
